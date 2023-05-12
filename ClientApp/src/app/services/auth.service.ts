701507f5a535289a44d6f8115f478e782be229aa
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-05-12 16:58:13 -0600

    x

--- ClientApp/src/app/services/auth.service.ts
+++ ClientApp/src/app/services/auth.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _authenticated: boolean
 
-  constructor() { }
+  constructor(@Inject(APP_BASE_HREF) private baseUrl: string, private _httpClient: HttpClient, ) { 
+    this._authenticated = false
+  }
   
+  //setter anf getter for acces token 
 }
