27fa08f09b39d61bf6002a481007e778e70bc932
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-06-16 16:33:55 -0600

    extending pagos table

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -15,14 +15,26 @@ namespace workcube_pagos.Models
         [IgnoreDataMember]
         public virtual Servicio Servicio    { get; set; }  
         public int IdServicio               { get; set; }
+        public string ServicioName          { get; set; }    
         [JsonIgnore]
         [IgnoreDataMember]
+
         public virtual Cliente Cliente      { get; set; }
         public int IdCliente                { get; set; }
+        public string ClienteName           { get; set; }
+        public string ClienteRazonSocial    { get; set; }
+        public string ClienteRFC            { get; set; }
+        public string ClienteDireccion      { get; set; }
+
         public string IdStripeCard          { get; set; }
+        public string TarjetaTipo           { get; set; }
+        public string TarjetaTerminacion    { get; set; } 
+        public string TarjetaBanco          { get; set; }
+
         public string IdStripeCharge        { get; set; }
         public long Monto                   { get; set; }
         public long Descuento               { get; set; }  
         public long Total                   { get; set; } 
+        public string CargoObj              { get; set; }   
     }
 }
