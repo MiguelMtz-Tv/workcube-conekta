fe9089f2da0546c4d50aaaf3b245441ad105fb1e
MiguelMtz-Tv <miguelayo128@gmail.com>
2023-06-30 13:50:06 -0600

    restored: DataService

--- ClientApp/src/app/services/data.service.ts
+++ ClientApp/src/app/services/data.service.ts
@@ -1,32 +1,19 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService implements OnDestroy {
+export class DataService {
   private dataSubject = new Subject<any>();
-  private isCalled = false;
-  private destroy$ = new Subject<void>();
 
   constructor() {}
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
 
   updateData(data: any) {
     this.dataSubject.next(data);
   }
 
   observeData(): Observable<any> {
-    if (!this.isCalled) {
-      this.isCalled = true;
-      return this.dataSubject.pipe(takeUntil(this.destroy$));
-    } else {
-      throw new Error('observeData() solo se puede llamar una vez en cada componente.');
-    }
+      return this.dataSubject;
   }
 }
